123df9d39ca6f96a6bcf6321fc847828c45f5e32
Jiseok CHOI <jiseok.dev@gmail.com>
2023-02-22 18:27:07 +0900

    Add `stable::block`

---
+++ stable/block/src/lib.rs
@@ -0,0 +1,33 @@
+#![allow(dead_code)]
+
+fn send(value: String) -> Result<(), SendError> {
+    println!("using {value} in a meaningful way");
+    // Simulate non-deterministic fallible action.
+    use std::time::SystemTime;
+    let period = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    if period.subsec_nanos() % 2 == 1 {
+        Ok(())
+    } else {
+        Err(SendError(value))
+    }
+}
+
+struct SendError(String);
+
+#[test]
+fn return_consumed_argument_on_error() {
+    let mut value = "imagine this is very long string".to_string();
+
+    let success = 's: {
+        // Try to send value two times.
+        for _ in 0..2 {
+            value = match send(value) {
+                Ok(()) => break 's true,
+                Err(SendError(value)) => value,
+            }
+        }
+        false
+    };
+
+    println!("success: {}", success);
+}
