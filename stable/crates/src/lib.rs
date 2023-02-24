86fb085c9d23e039e427917b2a3c2169a5aeba3e
Jiseok CHOI <jiseok.dev@gmail.com>
2023-02-25 01:13:40 +0900

    Add `stable::crates::serde_yaml`

--- stable/crates/src/lib.rs
+++ stable/crates/src/lib.rs
@@ -9,3 +9,50 @@ async fn get_public_ip() {
     let ip = public_ip::addr().await.unwrap();
     println!("public ip: {ip}");
 }
+
+#[test]
+fn yaml() {
+    use serde::{Deserialize, Deserializer, Serialize};
+
+    fn status_to_string<'de, D>(deserializer: D) -> Result<String, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = BookStatus::deserialize(deserializer)?;
+
+        Ok(match s {
+            BookStatus::Todo => "todo",
+            BookStatus::Doing => "doing",
+            BookStatus::Done => "done",
+        }
+        .to_owned())
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct Book {
+        title: String,
+        #[serde(deserialize_with = "status_to_string")]
+        status: String,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    enum BookStatus {
+        #[serde(rename = "todo")]
+        Todo,
+        #[serde(rename = "doing")]
+        Doing,
+        #[serde(rename = "done")]
+        Done,
+    }
+
+    let book = Book {
+        title: "The Rust Programming Language".to_owned(),
+        status: "todo".to_owned(),
+    };
+
+    let yaml = serde_yaml::to_string(&book).unwrap();
+    println!("{yaml}");
+
+    let book: Book = serde_yaml::from_str(&yaml).unwrap();
+    println!("{book:#?}");
+}
