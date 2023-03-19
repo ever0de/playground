436a532744e187a907be4afcdc17ebc4956b5300
Jiseok CHOI <jiseok.dev@gmail.com>
2023-03-19 13:11:43 +0900

    go/bloom-filters: impl delete

---
+++ go/bloom-filter/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"hash"
+	"hash/fnv"
+
+	"github.com/bits-and-blooms/bitset"
+)
+
+type BloomFilter struct {
+	bits        *bitset.BitSet
+	hashFuncs   []hash.Hash64
+	size        uint
+	numElements uint
+}
+
+// BloomFilter 생성
+func NewBloomFilter(size uint, numHashFuncs uint) *BloomFilter {
+	bf := &BloomFilter{
+		bits:      bitset.New(size),
+		hashFuncs: make([]hash.Hash64, numHashFuncs),
+		size:      size,
+	}
+	for i := range bf.hashFuncs {
+		bf.hashFuncs[i] = fnv.New64()
+	}
+	return bf
+}
+
+// BloomFilter에 요소 추가
+func (bf *BloomFilter) Add(element []byte) {
+	for _, hashFunc := range bf.hashFuncs {
+		hashFunc.Reset()
+		hashFunc.Write(element)
+		index := hashFunc.Sum64() % uint64(bf.size)
+		bf.bits.Set(uint(index))
+	}
+	bf.numElements++
+}
+
+// BloomFilter에서 요소 삭제
+func (bf *BloomFilter) Delete(element []byte) {
+	for _, hashFunc := range bf.hashFuncs {
+		hashFunc.Reset()
+		hashFunc.Write(element)
+		index := hashFunc.Sum64() % uint64(bf.size)
+		if bf.bits.Test(uint(index)) {
+			bf.bits.Clear(uint(index))
+			bf.numElements--
+		}
+	}
+}
+
+// BloomFilter에서 요소 존재 여부 확인
+func (bf *BloomFilter) Contains(element []byte) bool {
+	for _, hashFunc := range bf.hashFuncs {
+		hashFunc.Reset()
+		hashFunc.Write(element)
+		index := hashFunc.Sum64() % uint64(bf.size)
+		if !bf.bits.Test(uint(index)) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	bf := NewBloomFilter(100, 3)
+	bf.Add([]byte("hello"))
+	bf.Add([]byte("world"))
+	fmt.Println(bf.Contains([]byte("hello"))) // true
+	fmt.Println(bf.Contains([]byte("world"))) // true
+	bf.Delete([]byte("hello"))
+	fmt.Println(bf.Contains([]byte("hello"))) // false
+	fmt.Println(bf.Contains([]byte("world"))) // true
+}
--- go/go.mod
+++ go/go.mod
@@ -5,6 +5,7 @@ go 1.19
 require github.com/cockroachdb/pebble v0.0.0-20230306180248-e9a8c4ad65c5
 
 require (
+	github.com/bits-and-blooms/bitset v1.5.0 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
