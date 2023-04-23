7b2bbf805b495f3ffbacee73e5d3fc8b973fe325
Jiseok CHOI <jiseok.dev@gmail.com>
2023-04-23 16:02:06 +0900

    rust/proc-macro: wip

--- rust/stable/macros-expand/src/lib.rs
+++ rust/stable/macros-expand/src/lib.rs
@@ -4,13 +4,13 @@ use wasm_bindgen::prelude::wasm_bindgen;
 #[bind]
 pub enum EnumTemp {}
 
-// #[bind]
-// impl EnumTemp {
-//     #[bind]
-//     pub fn temp() -> u32 {
-//         42
-//     }
-// }
+#[bind]
+impl EnumTemp {
+    #[bind]
+    pub fn temp() -> u32 {
+        42
+    }
+}
 
-// #[bind]
-// pub struct StructTemp {}
+#[bind]
+pub struct StructTemp {}
